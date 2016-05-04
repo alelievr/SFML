af3c50e346b60e1316e0e71f3197a903ef8abc1c
alelievr <alelievr@student.42.fr>
2016-05-05 01:59:14 +0200

    removed SFML framework build

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
 	cmake -DCMAKE_BUILD_TYPE=Release && make
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-	cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=/usr/bin/clang++ -DCMAKE_C_COMPILER=/usr/bin/clang -DCMAKE_CXX_FLAGS="-stdlib=libc++" -DCMAKE_C_FLAGS="-stdlib=libc++" -DSFML_BUILD_FRAMEWORKS=1 . && make
+	cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=/usr/bin/clang++ -DCMAKE_C_COMPILER=/usr/bin/clang -DCMAKE_CXX_FLAGS="-stdlib=libc++" -DCMAKE_C_FLAGS="-stdlib=libc++" . && make
 elif [[ "$OSTYPE" == "cygwin" ]]; then
 	echo lol r u trying to run this on windows ?
 elif [[ "$OSTYPE" == "msys" ]]; then
