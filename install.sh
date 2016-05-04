997bd80ca5af69babf899da5563de0cd82fa2a60
alelievr <alelievr@student.42.fr>
2016-05-05 00:35:20 +0200

    added install.sh for mac osx and others platforms

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+	cmake -DCMAKE_BUILD_TYPE=Release && make
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+	cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=/usr/bin/clang++ -DCMAKE_C_COMPILER=/usr/bin/clang -DCMAKE_CXX_FLAGS="-stdlib=libc++" -DCMAKE_C_FLAGS="-stdlib=libc++" -DSFML_BUILD_FRAMEWORKS=1 . && make
+elif [[ "$OSTYPE" == "cygwin" ]]; then
+	echo lol r u trying to run this on windows ?
+elif [[ "$OSTYPE" == "msys" ]]; then
+	echo lol r u trying to run this on windows ?
+elif [[ "$OSTYPE" == "win32" ]]; then
+	echo lol win32 what else ?
+elif [[ "$OSTYPE" == "freebsd"* ]]; then
+	cmake -DCMAKE_BUILD_TYPE=Release && make
+else
+	echo uncknow distribution GG
+fi
